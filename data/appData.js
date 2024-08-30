9203fc109e9f9aa17a638aad3207506f05cef90b
Vachek-hooves <vachek0602@gmail.com>
2024-08-30 16:50:30 +0300

    create: hotels data

---
+++ data/appData.js
@@ -0,0 +1,184 @@
+export const AussieData = [
+  {
+    id: 'br',
+    city: 'Brisbane',
+    hotels: [
+      {
+        name: 'Stamford Plaza',
+        images: [
+          'https://cf.bstatic.com/xdata/images/hotel/max1024x768/245488057.jpg?k=743cd901cd3c54b929283bbaa763c2afa94f7411d55083c0f4e624062766d7da&o=&hp=1',
+          'https://cf.bstatic.com/xdata/images/hotel/max1024x768/245487344.jpg?k=05cbd1a90dc354f05f1802245fea37cffdbdf32c709ce30109ec6a8ce5fd3a8f&o=&hp=1',
+          'https://cf.bstatic.com/xdata/images/hotel/max1024x768/245488041.jpg?k=558f8d33b879098c97713f06669dd7ac59c2b7a2e99b2287258b7f664cb48383&o=&hp=1',
+          'https://cf.bstatic.com/xdata/images/hotel/max1024x768/245488038.jpg?k=dc06fefeee95b025144daa6e9ddc2f1152717fd61967f9b9452214d9e2da52a8&o=&hp=1',
+          'https://cf.bstatic.com/xdata/images/hotel/max1024x768/245488036.jpg?k=7212f9e5dd55c1322d406be07e1d2665a39bcd2863ac5baecde67b07552455cc&o=&hp=1',
+          'https://cf.bstatic.com/xdata/images/hotel/max1024x768/500341866.jpg?k=9f3ed46ee407bc43fe6d34799b18d81f748b299e540eecdb2dce86f4323d2797&o=&hp=1',
+        ],
+        description:
+          'Offering absolute riverfront accommodation and featuring river views from every room, Stamford Plaza Brisbane is perfectly positioned on the banks of the Brisbane River and is just a few steps from the adjacent Botanical Gardens. The hotel is located in the hub of the CBD (Central Business District) and within the city’s prestigious dining and shopping precinct. Self parking is available at an off-site location, 240 metres from the hotel.This unique riverfront property offers stunning vistas to the iconic Story Bridge and Kangaroo Point cliffs and lush open gardens. It boasts an onsite outdoor pool and spa, sauna, 24-hour fitness centre and 3 restaurants and bars.The refurbished guest rooms offer a tranquil and luxurious stay and feature a classic, elegant style.  All spacious, air-conditioned rooms include a flat-screen TV, free WiFi, mini bar, tea/coffee making facilities and a work space. The luxurious marble bathrooms feature a separate bath and walk-in shower. 24-hour in room dining is available.Guests can dine riverside on the promenade at The Brasserie on the River, or indoors for breakfast, lunch and dinner. The Brasserie offers a popular seafood buffet and delightful high tea. Kabuki Japanese features teppanyaki grill options or a la carte selections. The Pav Bar is the place for a light menu and cocktail while enjoying the open-air courtyard.',
+        address:
+          'Corner of Edward Street & Margaret Street, Brisbane CBD, 4000 Brisbane, Australia',
+        coordinates: {latitude: '-27.471410', longitude: '153.0308'},
+        price: {min: '172', max: '320'},
+      },
+      {
+        name: 'Hotel Grand Chancellor',
+        images: [
+          'https://cf.bstatic.com/xdata/images/hotel/max1024x768/557189318.jpg?k=d2e9ec31afd02943c9323374b1062d635d91e1259013f3cde4facd35e115b3f9&o=&hp=1',
+          'https://cf.bstatic.com/xdata/images/hotel/max1024x768/557189467.jpg?k=239d06bb702959cbe85b39bf849a0fc2aad472d916e16f0b3746404e1b22f52a&o=&hp=1',
+          'https://cf.bstatic.com/xdata/images/hotel/max1024x768/557189446.jpg?k=7eb4e62c5f1d02bc5896745f305057b34ce7bd67c08b9df973deb1e807a0b242&o=&hp=1',
+          'https://cf.bstatic.com/xdata/images/hotel/max1024x768/557189755.jpg?k=387a2585d70d038256fa547810f833f4971cd898305acb6b775b30e98e591a37&o=&hp=1',
+          'https://cf.bstatic.com/xdata/images/hotel/max1024x768/557190057.jpg?k=76957ce8a9bac1b4d80acef1ae9abc8f21f31c3f63d9b2fad3dbc3dea67e1839&o=&hp=1',
+          'https://cf.bstatic.com/xdata/images/hotel/max1024x768/557189452.jpg?k=fe85bec4aacf3e60c4730a2cf2a074fd6f7d4be6beed52519ee8bbe31161f58b&o=&hp=1',
+        ],
+        description:
+          'This 4.5-star hotel in the centre of Brisbane features a restaurant and bar with city views, rooftop swimming pool and fitness room (accessible by Stairwell only), plus spacious rooms with LCD TV and private balcony.Each room at Hotel Grand Chancellor Brisbane features a work desk with wireless internet access. Most rooms have panoramic views of the Roma Street Parklands or the city.Frescos Restaurant offers modern à la carte dining during the evening and extensive buffets for breakfast and lunch. Frescos Bar & Café serves light meals and a variety of drinks, including cocktails.Guests at The Grand Chancellor Brisbane can work out in the rooftop fitness centre, with advanced exercise equipment.Hotel Grand Chancellor Brisbane is only a 15-minute walk to Suncorp Stadium.',
+        address: '23 Leichhardt Street, Spring Hill, 4000 Brisbane, Australia ',
+        coordinates: {
+          latitude: '-27.4622',
+          longitude: '153.02184',
+        },
+        price: {min: '146', max: '408'},
+      },
+      {
+        name: 'Treasury Brisbane',
+        images: [
+          'https://cf.bstatic.com/xdata/images/hotel/max1024x768/106468871.jpg?k=2345709321d1af1083802dc1227b32d4413d969d225cc1634d6dd56a57c1c69e&o=&hp=1',
+          'https://cf.bstatic.com/xdata/images/hotel/max1024x768/106468631.jpg?k=05524143e261b3010f9c367821cfc1be0e0f766ca9047370e28da6c5212f34bb&o=&hp=1',
+          'https://cf.bstatic.com/xdata/images/hotel/max1024x768/106468485.jpg?k=66b5105343db28a81cf04464dfd333f9bfa3bba1e0684a1eed61a78967bce3b4&o=&hp=1',
+          'https://cf.bstatic.com/xdata/images/hotel/max1024x768/106468786.jpg?k=a7aa34208986b25ff1bc216716151a677b69fca85d2b1156e3c0e60f5b149860&o=&hp=1',
+          'https://cf.bstatic.com/xdata/images/hotel/max1024x768/77931922.jpg?k=322471776a21539285f7be70da256a1403d038608f5ba16bbd08410663705282&o=&hp=1',
+          'https://cf.bstatic.com/xdata/images/hotel/max1024x768/106468545.jpg?k=b1e25edba16291e91e97ceb3653bc43e0cc5f2b0297ac292ff4ff59cd3d7d4c3&o=&hp=1',
+        ],
+        description:
+          "The award-winning Treasury Casino & Hotel boasts luxurious rooms and suites offering old-world charm and modern comforts such as free WiFi. Situated just 200 metres from Brisbane River, this historic property offers 6 restaurants, 5 bars and a 24-hour reception desk.Housed in one of Australia's grandest heritage buildings, Treasury Casino & Hotel is less than 5 minutes' walk from Queen Street Mall's shopping, dining and nightlife. Brisbane Convention & Exhibition Centre, QPAC and Southbank Parklands are set just across the river, within 15 minutes’ walk. Brisbane Airport is a 25-minute drive away.Each of the elegant rooms feature high-ceilings, deluxe marble bathrooms and antique-style furnishings. All include a large flat-screen TV, an iPod docking station, an in-room safe and a minibar. Housekeeping services are offered twice a day.Additional guest facilities include conference and meeting rooms, self-parking for an additional charge of $30 per day and valet parking for an additional charge of $45 per day, laundry services and a fully equipped fitness centre with a sauna.",
+        address: '130 William St, Brisbane CBD, 4001 Brisbane, Australia',
+        coordinates: {
+          latitude: '-27.472545',
+          longitude: '153.02464485',
+        },
+        price: {min: '211', max: '270'},
+      },
+    ],
+  },
+  {
+    id: 'sy',
+    city: 'Sydney',
+    hotels: [
+      {
+        name: 'Emirates One&Only Wolgan Valley',
+        images: [
+          'https://dynamic-media-cdn.tripadvisor.com/media/photo-s/01/b7/16/c9/wolgan-valley-resort.jpg?w=600&h=-1&s=1',
+          'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0a/be/b2/0a/resort-grounds.jpg?w=1200&h=-1&s=1',
+          'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/0a/be/b2/05/signature-wildlife-and.jpg?w=1200&h=-1&s=1',
+          'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/28/9a/da/7d/your-special-private.jpg?w=1200&h=-1&s=1',
+          'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/28/9a/da/46/relish-just-baked-breads.jpg?w=1200&h=-1&s=1',
+          'https://dynamic-media-cdn.tripadvisor.com/media/photo-o/28/9a/d4/34/recline-with-loved-ones.jpg?w=1100&h=-1&s=1',
+        ],
+        description:
+          'Emirates One&Only Wolgan Valley is an ultra-luxury conservation-based resort, located 2.5 hours’ drive from Sydney (190kms) in the World Heritage-listed Greater Blue Mountains region. Set on 7,000 acres of carbon-neutral conservation and wildlife reserve, the resort combines absolute luxury with a quintessentially Australian bush experience. Occupying just one per cent of the total land area, the resort features 40 elegantly appointed villas reminiscent of the Federation period, each with its own swimming pool, double-sided fireplace and private verandah with stunning valley views. The resort offers a unique blend of seclusion, native wildlife, dramatic natural beauty, conservation and heritage combined with unsurpassed luxury facilities and outstanding food and wine. Emirates One&Only Wolgan Valley is part of One&Only Resorts and a member of Luxury Lodges of Australia.',
+        address:
+          '2600 Wolgan Rd, Wolgan Valley, New South Wales 2790 Australia',
+        coordinates: {latitude: '175', longitude: '320'},
+        price: {min: '-33.25212', max: '150.1960'},
+      },
+      {
+        name: 'Skye Suites Sydney',
+        images: [
+          'https://cf.bstatic.com/xdata/images/hotel/max1024x768/162204307.jpg?k=7513997834fcd507371ca6ede6485f6108d5dc3285315b31c1ec1d8b2d82f58a&o=&hp=1',
+          'https://cf.bstatic.com/xdata/images/hotel/max1024x768/162204889.jpg?k=1b7f9586c6bb7e9c3419c0d68c4e9037c33a1695dbdcd2a3ea15d954ca071980&o=&hp=1',
+          'https://cf.bstatic.com/xdata/images/hotel/max1024x768/162204841.jpg?k=757ba0dc57b01326707c45d3a0e68647ddb3331334067d0695a758cac78e4db3&o=&hp=1',
+          'https://cf.bstatic.com/xdata/images/hotel/max1024x768/162204833.jpg?k=6e54f1625dfa8698830961889bb013289a3b2c4f837f449c1a5c2f2a84f8763a&o=&hp=1',
+          'https://cf.bstatic.com/xdata/images/hotel/max1024x768/162204849.jpg?k=8d2c839c1fa1cf65b58ac33a94b80987b9328bdd4d5f391d23fb822cc6ceedff&o=&hp=1',
+          'https://cf.bstatic.com/xdata/images/hotel/max1024x768/162204558.jpg?k=a0a54893ba5211853e66a6180799ef02384dff30283a9faddbb7ebbf87b01317&o=&hp=1',
+        ],
+        description:
+          "Located in the heart of Sydney CBD (Central Business District), on Kent Street, SKYE Suites Sydney offers 73, designer boutique suites. Guests have access to an indoor pool, a hot tub and a fitness centre. Unlimited free WiFi is available.The apartment complex is 5 minutes’ walk from Barangaroo and the Queen Victoria Building. Darling Harbour, Pitt Street Mall and Westfield Shopping Centre are all within 10 minutes' walk. Kingsford Smith Airport is 8 km from the property.Each of the large, open plan suites offer plush bespoke bedding, a full kitchen/kitchenette and an internal laundry. Included in all rooms are Kevin Murphy bathroom amenities.",
+        address: '300 Kent Street, Sydney CBD, 2000 Sydney, Australia',
+        coordinates: {latitude: '-33.86766', longitude: '151.204471'},
+        price: {min: '222', max: '311'},
+      },
+      {
+        name: 'The Langham',
+        images: [
+          'https://cf.bstatic.com/xdata/images/hotel/max1024x768/113145057.jpg?k=326c3bb3fe4b5de44017b731daf1c6e29596214d2c4f503a0022c8094556ae49&o=&hp=1',
+          'https://cf.bstatic.com/xdata/images/hotel/max1024x768/222726859.jpg?k=a92c9868ca6ac77a409bc8bab7937107b33a96c662648c3dda27b24314afe8d5&o=&hp=1',
+          'https://cf.bstatic.com/xdata/images/hotel/max1024x768/13085500.jpg?k=8697465f587d77bcbcc4e30e2cd298ec9f44225571f74159a738a1ab0d7f77c8&o=&hp=1',
+          'https://cf.bstatic.com/xdata/images/hotel/max1024x768/43297814.jpg?k=024d74fd316b872f3a72e9ac230a798b14996164ab96a13e6e9511e7379c0d3f&o=&hp=1',
+          'https://cf.bstatic.com/xdata/images/hotel/max1024x768/33642634.jpg?k=d2b244e91e044915254c16c205da181d5ac9a6f46502d951f318159591e6139b&o=&hp=1',
+          'https://cf.bstatic.com/xdata/images/hotel/max1024x768/43297845.jpg?k=ecfe3f0b32f6802f830a2d6f35afed8de2a3988d488b9279a7e3092725980179&o=&hp=1',
+        ],
+        description:
+          'Just 5 minutes’ walk from Darling Harbour, The Langham Sydney offers luxurious accommodation with panoramic city or bay views. It features a day spa, a tennis court and a hot tub.The Langham Sydney is only 10 minutes’ walk from Circular Quay and The Rocks historic district. The Museum of Sydney is a 15-minute walk away.The stylishly decorated rooms feature mahogany furniture and a lavish marble bathroom. Each room boasts flat-screen satellite TV.Guests can take a dip in the swimming pool, or exercise in the well-equipped fitness centre. The sauna is the perfect place to unwind at the end of the day.In luxurious surrounds, the Observatory Bar offers some of the most unique and curious cocktails, the finest shared style food and a comprehensive and exclusive wine list, all served by the glass.Kitchens on Kent is bringing culinary excitement, theatre and extravagance to the city by redefining the concept of the traditional buffet. The elegant open-kitchen buffet experience will feature an array of interactive and live cooking stations where diners can observe and engage with the multicultural culinary experts. Enjoy the very best local produce, carefully prepared by Chefs from around the globe.',
+        address: '89-119 Kent Street, Sydney CBD, 2000 Sydney, Australia',
+        coordinates: {
+          latitude: '-33.86000',
+          longitude: '151.20352',
+        },
+        price: {min: '212', max: '417'},
+      },
+    ],
+  },
+  {
+    id: 'gc',
+    city: 'Gold Coast',
+    hotels: [
+      {
+        name: 'The Star Grand at The Star Gold Coast',
+        images: [
+          'https://cf.bstatic.com/xdata/images/hotel/max1024x768/152992164.jpg?k=cd7b2c5cc04563c4030c1bd9e10670b0088e0e70bce5b1123730f9ce5cdf1897&o=&hp=1',
+          'https://cf.bstatic.com/xdata/images/hotel/max1024x768/76370931.jpg?k=93c602c32d3ec9ccf0fca86b4a4470c3185400cfcf9909180323e0d87a348a3e&o=&hp=1',
+          'https://cf.bstatic.com/xdata/images/hotel/max1024x768/14029528.jpg?k=ea98f122b699835f2029b67caec91935980c49c50da4b0e0541f76e8b3ce33ad&o=&hp=1',
+          'https://cf.bstatic.com/xdata/images/hotel/max1024x768/91533534.jpg?k=450eecd91aabf3347134b3c506e3580270d26fa76ea3cd43043e779e259c81ba&o=&hp=1',
+          'https://cf.bstatic.com/xdata/images/hotel/max1024x768/91533545.jpg?k=dae94a6220500dac775e9f3f5acb15d241e50c684e6b3c824d0d3d7a4b9fa966&o=&hp=1',
+          'https://cf.bstatic.com/xdata/images/hotel/max1024x768/76377301.jpg?k=1725f3fd21f3aa8f5eb272ea020a590c9216e155977f09b7709e5e113bce4e4c&o=&hp=1',
+        ],
+        description:
+          "Located in the heart of Broadbeach on Australia's Gold Coast, The Star Grand at The Star Gold Coast is set amongst 7 acres of landscaped gardens and parklands.The Star Grand at The Star Gold Coast features a wide range of restaurants and bars and a 24/hour casino. Guests can enjoy a swim in the outdoor pool.All rooms feature individual climate control, cable TV and an in-room safe. Each room has a work desk with high speed broadband or wireless internet, plus movie and gaming channels on the flat-screen TV.Guests can take a short stroll to the shops and restaurants of Broadbeach. The beach is only a 10-minute walk from The Star Grand at The Star Gold Coast.",
+        address: '8 Casino Drive, Broadbeach, 4218 Gold Coast, Australia',
+        coordinates: {
+          latitude: '-28.0309476',
+          longitude: '153.428311',
+        },
+        price: {min: '185', max: '293'},
+      },
+      {
+        name: 'The Island Gold Coast',
+        images: [
+          'https://cf.bstatic.com/xdata/images/hotel/max1024x768/145053212.jpg?k=6512c6611824a56d14546ab841bae0bf74c5a7def2dc1ddc8b6083b1ac8079de&o=&hp=1',
+          'https://cf.bstatic.com/xdata/images/hotel/max1024x768/107117342.jpg?k=b5ab80207327ac4c9978867dc23dedf2e0aa23410fd734f1a82eab9b95670f3d&o=&hp=1',
+          'https://cf.bstatic.com/xdata/images/hotel/max1024x768/309686733.jpg?k=42698f97043fb5af235187f9bc4820ada74e3f56a4df4892c1ae5903e01f5ef3&o=&hp=1',
+          'https://cf.bstatic.com/xdata/images/hotel/max1024x768/309686748.jpg?k=b0f8550a40f1a2d47553e1f4c5871e142405d428be1ce2d4684624bca6428eb3&o=&hp=1',
+          'https://cf.bstatic.com/xdata/images/hotel/max1024x768/309686809.jpg?k=395468decffe50bcd8596df5120c3fc74c80583bb5b828c354a893cd1d9bc768&o=&hp=1',
+        ],
+        description:
+          'Located in the heart of Surfers Paradise, The Island offers modern boutique accommodation with the popular Rooftop Bar and vibrant restaurant and bar, Goldie. Guests can relax on the sun terrace, or enjoy a swim in the pool. The vibrant bars, restaurants and shops of the Gold Coast are all within a 5-minute walk. Free WiFi is provided.Each air conditioned room has a balcony with either city, mountain or pool views. There is a flat-screen TV and a kettle. The private bathroom includes free toiletries and a hairdryer. The Casino is 3.2 km from The Island, while Pacific Fair Shoping Centre is 3.5 km away. The nearest airport is Gold Coast Airport, 20 km from The Island.',
+        address:
+          '3128 Surfers Paradise Boulevard, Surfers Paradise, 4217 Gold Coast, Australia',
+        coordinates: {
+          latitude: '-28.002463',
+          longitude: '153.42832',
+        },
+        price: {min: '135', max: '173'},
+      },
+      {
+        name: 'Vibe Hotel Gold Coast',
+        images: [
+          'https://cf.bstatic.com/xdata/images/hotel/max1024x768/398467163.jpg?k=6e6ba57b64695e14918ade79a03bc735033a1b21bdb527b1dbd52e9390d8b522&o=&hp=1',
+          'https://cf.bstatic.com/xdata/images/hotel/max1024x768/404129002.jpg?k=2dac7cfe392677951668011ff3c925bb7c43dc49ceeeb031e739b332d8650d9c&o=&hp=1',
+          'https://cf.bstatic.com/xdata/images/hotel/max1024x768/404129045.jpg?k=967847f8f81970de7b605de121c8badab79aef794615d8e33953d1503fe23164&o=&hp=1',
+          'https://cf.bstatic.com/xdata/images/hotel/max1024x768/404128993.jpg?k=1a1eb555b6245b2bd1c6b06d8076bb2dfa03535aed0a7e7b8cd4ef4cfe6a268d&o=&hp=1',
+          'https://cf.bstatic.com/xdata/images/hotel/max1024x768/404128976.jpg?k=5f558787661c28a3029b2bd7d7f3d64c3a40771a1412df973d4065678d080e98&o=&hp=1',
+        ],
+        description:
+          "Overlooking the Nerang River, the modern and contemporary Vibe Hotel Gold Coast features a swimming pool and parking, 5 minutes' walk from Surfers Paradise Beach. Seaworld is less than 10 minutes’ drive from the hotel. The Dreamworld, Movie World and Wet 'n' Wild theme parks are within a 25-minute drive.All rooms at Hotel Vibe Gold Coast are modern and bright. Some rooms offer wonderful views over the river or the ocean from the balcony. Rooms with a sofa and kitchenette are also available.The Driftwood Social Club offers beautiful views of the river and the pool, and is open daily for breakfast, lunch, dinner and cocktails.Perfectly located in the vibrant and exciting heart of Surfers Paradise, Vibe Hotel Gold Coast offers guests access to sun-drenched golden beaches, rolling surf, exciting nightlife and a perfect climate.",
+        address:
+          '42 Ferny Avenue, Surfers Paradise, 4217 Gold Coast, Australia',
+        coordinates: {
+          latitude: '-27.998789',
+          longitude: '153.426384',
+        },
+        price: {min: '133', max: '298'},
+      },
+    ],
+  },
+];
